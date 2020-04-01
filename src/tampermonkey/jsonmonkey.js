5294f283eaa564c9c03f7efe4a4d0d4f0dee40a1
Jason Seegmiller <jasonseegmiller@overstock.com>
2020-03-31 23:18:52 -0600

    Fetching breakdowns instead

--- src/tampermonkey/jsonmonkey.js
+++ src/tampermonkey/jsonmonkey.js
@@ -23,45 +23,28 @@ const getRewardsTotal = () => {
     return rewards ? Number(rewards.innerText) : 0;
 };
 
-const getSearchBreakdowns = () => {
-    // OLD METHOD
-    // const breakdown = { desktop: {}, mobile: {} }
-    // const points = document.getElementsByClassName('vsrewds_searchpoints');
-    // const curMax = (str) => {
-    //     const nums = str.split(' of ');
+const getSearchBreakdowns = async () => {
+    // TODO: replace ridiculous iframe drilling to just a fetch to /rewardsapp/bepflyoutpage?style=modular&date=03/31/2020
+    const breakdowns = await fetch(`/rewardsapp/bepflyoutpage?style=modular&date=${new Date().toLocaleDateString()}`)
+        .then((response) => response.text())
+        .then((text) => {
+            const breakdown = {};
+            const parser = new DOMParser();
+            const htmlDocument = parser.parseFromString(text, 'text/html');
+            const items = htmlDocument.documentElement.querySelector('#credits .breakdown').children;
+            for (let i = 0; i < items.length; i++) {
+                const nums = items[i].innerText.split('/');
 
-    //     return { current: nums[0], max: nums[1] };
-    // }
+                breakdown[items[i].className] = {
+                    current: Number(nums[0]),
+                    max: Number(nums[1]),
+                };
+            }
 
-    // for (var i = 0; i < points.length; i++) {
-    //     if (points[i].previousElementSibling.innerText === 'PC Search Daily Points') {
-    //         breakdown.desktop = curMax(points[i].innerText);
-    //     } else if (points[i].previousElementSibling.innerText === 'Mobile Search Daily Points') {
-    //         breakdown.mobile = curMax(points[i].innerText);
-    //     }
-    // }
+            return breakdown;
+        });
 
-    // breakdown.edge = { current: null, max: null };
-
-    const breakdown = {};
-    document.getElementById('id_rh').click();
-
-    // TODO: wait till iframe loads instead (#bepfm or #bepfo)
-    // w.addEventListener('load', () => {}, true);
-    setTimeout(() => {
-        const items = document.querySelectorAll('#credits .breakdown')[0].children;
-
-        for (let i = 0; i < items.length; i++) {
-            const nums = items[i].innerText.split('/');
-
-            breakdown[items[i].className] = {
-                current: Number(nums[0]),
-                max: Number(nums[1]),
-            };
-        }
-    }, 3000);
-
-    return breakdown;
+    return breakdowns;
 };
 
 const getSearchLinks = () => {
@@ -86,8 +69,8 @@ const search = () => {
         const newLink = storedLinks.pop();
 
         setTimeout(() => {
-            GM_openInTab(newLink, true /* open in background */);
-            // window.href(newLink);
+            GM_log(`Heading to: ${newLink}`);
+            GM_openInTab(newLink, false /* open in background */);
         }, 1500);
 
         if (storedLinks.length === 0) {
@@ -116,21 +99,22 @@ window.addEventListener('load', () => {
             search();
         } else {
             // Get breakdowns
-            let breakdown;
-            setTimeout(() => {
-                breakdown = JSON.stringify(getSearchBreakdowns(), null, 4);
-            }, 3000);
+            // const { desktop } = async () => {
+            //     setTimeout(() => JSON.stringify(, null, 4), 3000);
+            // };
+            getSearchBreakdowns()
+                .then((breakdown) => {
+                    GM_log(`Breakdown: ${breakdown.desktop}`); // undefined
 
-            GM_log(`Breakdown: ${breakdown}`); // undefined
-
-            // See if we've maxed out and should stop searching
-            if (breakdown.desktop.current < breakdown.desktop.max) {
-                GM_setValue(searchCountKey, searchCount += 1);
-                search();
-            } else {
-                GM_log('Setting searchCountKey to 0!');
-                GM_setValue(searchCountKey, 0);
-            }
+                    // See if we've maxed out and should stop searching
+                    if (breakdown.desktop.current < breakdown.desktop.max) {
+                        GM_setValue(searchCountKey, searchCount += 1);
+                        search();
+                    } else {
+                        GM_log('Setting searchCountKey to 0!');
+                        GM_setValue(searchCountKey, 0);
+                    }
+                });
         }
     } else {
         GM_log('You are NOT signed in, please sign in to automate.');
